0c7ba277320dc447b7e9903c1f71bca9cbc1af33
baekteun <baegteun@gmail.com>
2021-10-08 09:59:11 +0900

    [백준] 11279 최대 힙 - Swift
    시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
    1 초 (추가 시간 없음) (하단 참고)	256 MB	32529	14077	10816	45.197%
    문제
    널리 잘 알려진 자료구조 중 최대 힙이 있다. 최대 힙을 이용하여 다음과 같은 연산을 지원하는 프로그램을 작성하시오.
    
    배열에 자연수 x를 넣는다.
    배열에서 가장 큰 값을 출력하고, 그 값을 배열에서 제거한다.
    프로그램은 처음에 비어있는 배열에서 시작하게 된다.
    
    입력
    첫째 줄에 연산의 개수 N(1 ≤ N ≤ 100,000)이 주어진다. 다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다. 만약 x가 자연수라면 배열에 x라는 값을 넣는(추가하는) 연산이고, x가 0이라면 배열에서 가장 큰 값을 출력하고 그 값을 배열에서 제거하는 경우이다. 입력되는 자연수는 231보다 작다.
    
    출력
    입력에서 0이 주어진 회수만큼 답을 출력한다. 만약 배열이 비어 있는 경우인데 가장 큰 값을 출력하라고 한 경우에는 0을 출력하면 된다.
    
    예제 입력 1
    13
    0
    1
    2
    0
    0
    3
    2
    1
    0
    0
    0
    0
    0
    예제 출력 1
    0
    2
    1
    3
    2
    1
    0
    0

---
+++ Baekjoon-Swift/B_11279/B_11279.swift
@@ -0,0 +1,121 @@
+//
+//  main.swift
+//  Baekjoon-Swift
+//
+//  Created by baegteun on 2021/10/08.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+var q = PriorityQueue<Int>(){
+    return $0 >= $1
+}
+var res = ""
+for _ in 0..<n{
+    let x = Int(readLine()!)!
+    guard x != 0 else{
+        res += "\(q.pop() ?? 0)\n"
+        continue
+    }
+    q.push(x)
+}
+print(res)
+
+
+class PriorityQueue<T> {
+    private var heap: [T] = []
+    private let comparing: (_ o1: T,_ o2: T) -> Bool
+    init(_ comparing: @escaping (_ o1: T,_ o2: T) -> Bool) {
+        self.comparing = comparing
+        
+    }
+    func size() -> Int {
+        heap.count
+        
+    }
+    func isEmpty() -> Bool {
+        heap.isEmpty
+        
+    }
+    func clear() {
+        heap.removeAll()
+        
+    }
+    func peek() -> T? {
+        heap.first
+        
+    }
+    func push(_ value: T) {
+        heap.append(value)
+        if heap.count == 1 {
+            return
+            
+        }
+        var valueIndex = heap.count - 1
+        var parentIndex = (valueIndex-1) / 2
+        while !comparing(heap[parentIndex], heap[valueIndex]) {
+            heap.swapAt(valueIndex, parentIndex)
+            valueIndex = parentIndex
+            parentIndex = (valueIndex-1) / 2
+            if valueIndex == 0 {
+                break
+                
+            }
+            
+        }
+        
+    }
+    func pop() -> T? {
+        if heap.count == 0 {
+            return nil
+            
+        }
+        if heap.count == 1 {
+            return heap.removeFirst()
+            
+        }
+        func isChildEmpty(_ value: Int,_ left: Int,_ right: Int) -> Bool {
+            if heap.count <= left {
+                return true
+                
+            }
+            if heap.count > right {
+                return false
+                
+            }
+            if comparing(heap[value], heap[left]) {
+                return true
+                
+            }
+            heap.swapAt(value, left)
+            return true
+            
+        }
+        heap.swapAt(0, heap.count-1)
+        let answer = heap.removeLast()
+        var valueIndex = 0
+        var leftIndex = valueIndex * 2 + 1
+        var rightIndex = valueIndex * 2 + 2
+        if isChildEmpty(valueIndex, leftIndex, rightIndex) {
+            return answer
+            
+        }
+        
+        while !comparing(heap[valueIndex], heap[leftIndex]) || !comparing(heap[valueIndex], heap[rightIndex]) {
+            let index = comparing(heap[leftIndex], heap[rightIndex]) ? leftIndex : rightIndex
+            heap.swapAt(valueIndex, index)
+            valueIndex = index
+            leftIndex = valueIndex * 2 + 1
+            rightIndex = valueIndex * 2 + 2
+            if isChildEmpty(valueIndex, leftIndex, rightIndex) {
+                break
+                
+            }
+            
+        }
+        return answer
+        
+    }
+    
+}
