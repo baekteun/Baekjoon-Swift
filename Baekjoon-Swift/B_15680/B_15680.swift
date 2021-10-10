48fc93d92c7f86641f1272ebf316fe6908d2b196
baekteun <baegteun@gmail.com>
2021-10-10 14:53:39 +0900

    [백준] 16580 연세대학교 - Swift
    시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
    1 초	128 MB	5540	4074	3764	75.130%
    문제
    
    연세대학교의 영문명은 YONSEI, 슬로건은 Leading the Way to the Future이다.
    
    이를 출력하는 프로그램을 작성해보도록 하자.
    
    입력
    첫째 줄에 N이 주어진다. (N = 0 또는 1)
    
    출력
    N = 0일 경우: 연세대학교의 영문명을 출력한다.
    N = 1일 경우: 연세대학교의 슬로건을 출력한다.
    대소문자 구별에 주의하도록 하자.
    
    예제 입력 1
    0
    예제 출력 1
    YONSEI

---
+++ Baekjoon-Swift/B_15680/B_15680.swift
@@ -0,0 +1,14 @@
+//
+//  B_15680.swift
+//  Baekjoon-Swift
+//
+//  Created by baegteun on 2021/10/10.
+//
+
+import Foundation
+let inp = readLine()!
+if inp=="1"{
+    print("Leading the Way to the Future")
+}else{
+    print("YONSEI")
+}
