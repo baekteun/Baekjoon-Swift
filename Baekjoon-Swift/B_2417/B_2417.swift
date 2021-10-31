2ad2866eafd35e9595242e64caebf1a99383939a
baekteun <baegteun@gmail.com>
2021-10-31 13:20:26 +0900

    [백준] 2417 정수 제곱근 - Swift
    시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
    1 초	128 MB	4365	1647	1386	41.385%
    문제
    정수가 주어지면, 그 수의 정수 제곱근을 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 정수 n이 주어진다. (0 ≤ n < 263)
    
    출력
    첫째 줄에 q2 ≥ n인 가장 작은 음이 아닌 정수 q를 출력한다.
    
    예제 입력 1
    122333444455555
    예제 출력 1
    11060446

---
+++ Baekjoon-Swift/B_2417/B_2417.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Baekjoon-Swift
+//
+//  Created by baegteun on 2021/10/31.
+//
+
+import Foundation
+
+let input = UInt64(readLine()!)!
+
+print(Int(ceil(sqrt(Double(input)))))
