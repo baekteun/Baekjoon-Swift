0b7759f1e7344cef44dda6767a356f502a8578e3
baegteun <baegteun@gmail.com>
2021-12-13 08:36:41 +0900

    [백준] 20492 세금 - Swift
    시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
    1 초	1024 MB	3601	2286	2133	63.901%
    문제
    선린인터넷고등학교의 한 학생은 프로그래밍 대회에 참가하여 거액의 상금을 수상하는 영광을 누리게 되었다. 하지만, 이 학생이 상금 금액의 전부를 수령하게 되는 것은 아니다. 상금의 일부를 제세공과금으로 납부하고, 나머지 금액을 수령하게 된다.
    
    일반적으로, 대회에서 상금을 받으면 전체 금액의 22%를 제세공과금으로 국가에 납부하고, 나머지 금액을 수령하게 된다. 하지만, 상금의 80%를 필요 경비로 인정하게 되면, 나머지 20% 중 22%만을 제세공과금으로 납부해도 된다.
    
    대회 상금의 금액이 주어질 때, 다음 두 경우 각각에 대해 이 학생이 실제로 수령하는 금액을 구해보자.
    
    전체 상금의 22%를 제세공과금으로 납부하는 경우
    상금의 80%를 필요 경비로 인정받고, 나머지 금액 중 22%만을 제세공과금으로 납부하는 경우
    입력
    상금의 금액을 나타내는 하나의 정수
    N
    $N$이 주어진다.
    
    출력
    1번 경우에 대한 답과 2번 경우에 대한 답을 사이에 공백을 두고 출력한다.
    
    제한
     
    1
    000
    ≤
    N
    ≤
    10
    000
    000
    $1\,000 \le N \le 10\,000\,000$ 
     
    N
    $N$은
    1
    000
    $1\,000$의 배수
    예제 입력 1  복사
    10000000
    예제 출력 1  복사
    7800000 9560000
    예제 입력 2  복사
    1000
    예제 출력 2  복사
    780 956

---
+++ Baekjoon-Swift/B_20492/B_20492.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  Baekjoon-Swift
+//
+//  Created by 최형우 on 2021/12/13.
+//
+
+let input = Int(readLine()!)!
+print("\(Int(Double(input) - Double(input)*0.22)) \( input - Int(Double(Double(input) * 0.2) * 0.22) )")
