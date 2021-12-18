b41c071dfb34a6716bc22d709139115d03d081eb
baegteun <baegteun@gmail.com>
2021-12-19 08:05:30 +0900

    [백준] 2442 별 찍기 - 5 - Swift
    시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
    1 초	128 MB	38827	21802	19452	56.586%
    문제
    첫째 줄에는 별 1개, 둘째 줄에는 별 3개, ..., N번째 줄에는 별 2×N-1개를 찍는 문제
    
    별은 가운데를 기준으로 대칭이어야 한다.
    
    입력
    첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
    
    출력
    첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
    
    예제 입력 1  복사
    5
    예제 출력 1  복사
        *
       ***
      *****
     *******
    *********

---
+++ Baekjoon-Swift/B_2442/B_2442.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Baekjoon-Swift
+//
+//  Created by 최형우 on 2021/12/19.
+//
+
+guard let n = Int(readLine()!) else { fatalError() }
+for i in 0..<n{
+    for _ in 0..<n-i-1 {print(" ",terminator: "")}
+    for _ in 0..<2*i+1 {print("*",terminator: "")}
+    print("\n")
+}
