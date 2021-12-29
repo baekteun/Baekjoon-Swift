64cdca76dd20d75ecf177c54d2b2a71808fe4082
baegteun <baegteun@gmail.com>
2021-12-29 09:38:17 +0900

    [백준] 16430 제리와 톰 - Swift
    시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
    1 초	128 MB	5167	4372	4127	85.640%
    문제
    톰은 마트에서 치즈 1kg 을 사서 집으로 돌아왔습니다.
    
    그런데 톰이 한눈을 판 사이 제리가 와서 A/B kg 만큼 훔쳐갔습니다.
    
    제리가 치즈를 훔쳐 간 후 톰이 가지고 있는 치즈의 무게는 얼마인가요?
    
    입력
    첫 번째 줄에 두 정수 A, B (1 ≤ A < B ≤ 9) 가 주어집니다.
    
    A와 B는 서로소임이 보장됩니다.
    
    출력
    정답을 기약분수로 표현했을 때 P/Q kg 이라면 첫 번째 줄에 P와 Q를 공백을 사이에 두고 출력합니다.
    
    예제 입력 1  복사
    2 7
    예제 출력 1  복사
    5 7
    예제 입력 2  복사
    5 8
    예제 출력 2  복사
    3 8

---
+++ Baekjoon-Swift/B_16430/B_16430.swift
@@ -0,0 +1,9 @@
+//
+//  B_16430.swift
+//  Baekjoon-Swift
+//
+//  Created by 최형우 on 2021/12/29.
+//
+
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+print(input[1]-input[0], input[1])
