4e45cc887aa40615ed38a3e4b0f197391974d5e2
baegteun <baegteun@gmail.com>
2026-02-27 23:07:42 +0900

    25314 코딩은 체육과목 입니다

---
+++ B_25314/B_25314/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  B_25314
+//
+//  Created by baegteun on 2/27/26.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+let longString = String(repeating: "long ", count: n / 4)
+print("\(longString)int")
